ac3c8a69dbfed7878eacb41daa5a9d37de9a1e7f
Sopiriye Robinson <sopiriyerobinson@gmail.com>
2026-04-06 15:31:13 +0100

    implement: the decorator for the current user was implement which is important for the logout controller

---
+++ src/auth/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { AuthenticatedUser } from './types/authenticated-user.interface';
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext): AuthenticatedUser => {
+    const request = context
+      .switchToHttp()
+      .getRequest<{ user: AuthenticatedUser }>();
+    return request.user;
+  },
+);
