f392526af1f3da15bacd44e0c3ddaa89af06ed8c
Sopiriye Robinson <sopiriyerobinson@gmail.com>
2026-04-17 11:28:47 +0100

    deplpoyment trial 9

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+// import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
